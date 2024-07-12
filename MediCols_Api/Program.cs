5b10bcafa9eccf600a8b4ab666dd4e8efcd37ec6
JuanDiego <cruzcf1297@gmail.com>
2024-07-12 12:03:10 -0500

    Cambio validacion Token

--- MediCols_Api/Program.cs
+++ MediCols_Api/Program.cs
@@ -48,7 +48,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors();
 app.UseHttpsRedirection();
-app.UseAuthorization();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
